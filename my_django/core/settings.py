eff25668eac2f959d6c2d880ea4ddc721a1237bd
Loanshark <63855173+changhyuni@users.noreply.github.com>
2020-10-25 19:22:28 +0900

    Update settings.py

--- my_django/core/settings.py
+++ my_django/core/settings.py
@@ -123,8 +123,8 @@ USE_TZ = True
 # STATIC_URL = '/static/'
 # STATICFILES_DIRS = (os.path.join('static'), )
 
-AWS_ACCESS_KEY_ID = 'AKIAW5P72PBSXJZXRYMG'
-AWS_SECRET_ACCESS_KEY = '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'
+AWS_ACCESS_KEY_ID = 'xxxxxxxxxxxxxxxx'
+AWS_SECRET_ACCESS_KEY = 'xxxxxxxxxxxxxxxxxxx'
 AWS_STORAGE_BUCKET_NAME = 'seunghyeon-project-original'
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
